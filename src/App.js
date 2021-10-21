53715288215e3bbfa5def35bb21abeda3f261f6e
sharmaji-2407 <prakharksharma@gmail.com>
2021-10-22 00:59:19 +0530

    adding main App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Web3 from 'web3';
+
+
+
+
+
+
+
 
 function App() {
+  const [mounted, setMounted] = useState(false)
+  const [account, setaccount] = useState("");
+  
+  const LoadBlockChainData = async() =>
+  {
+  //<----- Old way --->
+  // if (window.ethereum) 
+  // {
+  //   const web3 = new Web3(window.ethereum);
+  //   // Ask User permission to connect to Metamask
+  //   await window.ethereum.enable();
+  // };
+
+  //<---- New Way to connect to metamask ---->
+  if (window.ethereum) 
+  {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        //setaccount(accounts);
+      } catch (error) {
+        if (error.code === 4001) {
+          // User rejected request
+        }
+      }
+    }
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const network = await web3.eth.net.getNetworkType()
+    const accounts = await web3.eth.getAccounts();
+    //console.log("account", accounts[0])
+    setaccount(accounts);
+  }
+    
+  
+  if(!mounted)
+    {
+    // Code for componentWillMount here
+    LoadBlockChainData()
+    }
+    useEffect(() =>{setMounted(true)},[])
+
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>E Voting Dapp</h1>
+      <p>Your Account : {account} </p>
     </div>
   );
 }
 
 export default App;
+
+
+
+ 
+
+
+
+
