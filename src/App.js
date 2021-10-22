a195025405c339fe1fa86e67d012911b53d27c29
sharmaji-2407 <prakharksharma@gmail.com>
2021-10-22 20:28:50 +0530

    App.js and A.css updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Web3 from 'web3';
-
+import NavBar from './NavBar';
 
 
 
@@ -54,7 +54,9 @@ function App() {
 
   
   return (
+    
     <div className="container">
+    <NavBar />
       <h1>E Voting Dapp</h1>
       <p>Your Account : {account} </p>
     </div>
